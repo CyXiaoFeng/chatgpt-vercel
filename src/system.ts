e660029601aed9db1a97cdb31dd853b9d79a1913
Kirk <2420958617@qq.com>
2023-03-30 16:35:32 +0800

    init

--- src/system.ts
+++ src/system.ts
@@ -3,7 +3,7 @@ import type { Model } from "./types"
 export const defaultSetting = {
   continuousDialogue: true,
   archiveSession: false,
-  openaiAPIKey: "",
+  openaiAPIKey: "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",
   openaiAPITemperature: 60,
   password: "",
   systemRule: "",
